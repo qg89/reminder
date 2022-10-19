e4cca8de9c51bfe7de1be975e1fde2c0977b7efe
齐钢 <qigang@mxnavi.com>
2022-10-19 09:20:51 +0800

    add CoverityApi

--- src/main/java/com/q/reminder/reminder/handle/UpdateFeishuRedmineHandle.java
+++ src/main/java/com/q/reminder/reminder/handle/UpdateFeishuRedmineHandle.java
@@ -38,8 +38,8 @@ public class UpdateFeishuRedmineHandle {
     @Value("${app.secret}")
     private String appSecret;
 
-//    @Scheduled(cron = "0 0 23 ? * MON-FRI")
-    @Scheduled(cron = "0/20 * * * * ?")
+    @Scheduled(cron = "0 0 23 ? * MON-FRI")
+//    @Scheduled(cron = "0/20 * * * * ?")
     public void update() {
         String secret = FeiShuApi.getSecret(appId, appSecret);
         List<GroupInfo> groupToChats = FeiShuApi.getGroupToChats(secret);
