3b0671caea09233484a8c904569f2505b80ca286
齐钢 <qigang@mxnavi.com>
2022-10-19 12:19:33 +0800

    add CoverityApi

--- src/main/java/com/q/reminder/reminder/handle/OverdueTasksAgain1ToGroupHandle.java
+++ src/main/java/com/q/reminder/reminder/handle/OverdueTasksAgain1ToGroupHandle.java
@@ -1,14 +1,20 @@
 package com.q.reminder.reminder.handle;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.q.reminder.reminder.entity.NoneStatus;
 import com.q.reminder.reminder.handle.base.OverdueTasksAgainToGroupBase;
-import com.q.reminder.reminder.handle.base.QueryTasksToMemberBase;
 import com.q.reminder.reminder.service.NoneStatusService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author : saiko
@@ -26,12 +32,26 @@ public class OverdueTasksAgain1ToGroupHandle {
     @Autowired
     private NoneStatusService noneStatusService;
 
-//    @Scheduled(cron = "0 30 9 * * ?")
-    @Scheduled(cron = "0/20 * * * * ?")
+    @Scheduled(cron = "0 30 9 * * ?")
+//    @Scheduled(cron = "0/20 * * * * ?")
     public void query() {
-        List<String> noneStatusList = noneStatusService.queryUnInStatus(2);
-        // 组装数据， 通过人员，获取要发送的内容
-        overdueTasksAgainToGroupBase.overdueTasksAgainToGroup(1, noneStatusList, Boolean.FALSE);
-//        overdueTasksAgainToGroupBase.overdueTasksAgainToGroup(2, noneStatusList, Boolean.TRUE);
+        LambdaQueryWrapper<NoneStatus> lq = new LambdaQueryWrapper<>();
+        lq.in(NoneStatus::getExpiredDays, 1, 2);
+        List<NoneStatus> noneStatusList = noneStatusService.list(lq);
+        Map<String, List<NoneStatus>> statusMap = noneStatusList.stream().collect(Collectors.groupingBy(e -> String.valueOf(e.getExpiredDays())));
+        List<String> noneStatusStrList = new ArrayList<>();
+        if (statusMap.containsKey("1")) {
+           noneStatusStrList = statusMap.get("1").stream().map(NoneStatus::getNoneStatus).collect(Collectors.toList());
+        }
+        if (!CollectionUtils.isEmpty(noneStatusList)) {
+            // 组装数据， 通过人员，获取要发送的内容
+            overdueTasksAgainToGroupBase.overdueTasksAgainToGroup(1, noneStatusStrList, Boolean.FALSE);
+        }
+        if (statusMap.containsKey("2")) {
+            noneStatusStrList = statusMap.get("2").stream().map(NoneStatus::getNoneStatus).collect(Collectors.toList());
+        }
+        if (!CollectionUtils.isEmpty(noneStatusList)) {
+            overdueTasksAgainToGroupBase.overdueTasksAgainToGroup(2, noneStatusStrList, Boolean.TRUE);
+        }
     }
 }
