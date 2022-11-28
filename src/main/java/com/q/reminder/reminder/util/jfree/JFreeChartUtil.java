4504cd5997368a2674fc9d69998b59a0cc09fbb2
齐钢 <qigang@mxnavi.com>
2022-11-28 16:52:01 +0800

    add CoverityApi

--- src/main/java/com/q/reminder/reminder/util/jfree/JFreeChartUtil.java
+++ src/main/java/com/q/reminder/reminder/util/jfree/JFreeChartUtil.java
@@ -75,14 +75,14 @@ public class JFreeChartUtil {
      */
     public static Font getDefaultFont(int style, Float size) throws Exception {
         //获取宋体文件
-        URL url = JFreeChartUtil.class.getClassLoader().getResource("templates/font/msyh.ttc");
-        //文件路径
-        File defaultFontFile = new File(url.getFile());
-        Path path = defaultFontFile.toPath();
-        if (Files.isReadable(path) && Files.exists(path) && Files.isRegularFile(path)) {
-            Font defaultFont = Font.createFont(Font.TRUETYPE_FONT, defaultFontFile);
-            return defaultFont.deriveFont(style, size);
-        }
+//        URL url = JFreeChartUtil.class.getClassLoader().getResource("templates/font/msyh.ttc");
+//        //文件路径
+//        File defaultFontFile = new File(url.getFile());
+//        Path path = defaultFontFile.toPath();
+//        if (Files.isReadable(path) && Files.exists(path) && Files.isRegularFile(path)) {
+//            Font defaultFont = Font.createFont(Font.TRUETYPE_FONT, defaultFontFile);
+//            return defaultFont.deriveFont(style, size);
+//        }
         return new Font("微软雅黑", Font.PLAIN, 15).deriveFont(style, size);
     }
 
