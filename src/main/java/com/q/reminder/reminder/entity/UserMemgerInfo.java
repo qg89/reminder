238fe802a266d46a59ad2e5deb88a43d913418dd
齐钢 <qigang@mxnavi.com>
2022-12-01 15:48:09 +0800

    add CoverityApi

--- src/main/java/com/q/reminder/reminder/entity/UserMemgerInfo.java
+++ src/main/java/com/q/reminder/reminder/entity/UserMemgerInfo.java
@@ -36,10 +36,6 @@ public class UserMemgerInfo implements Serializable {
     @JSONField(name = "member_id_type")
     private String memberIdType;
     /**
-     * redmineID
-     */
-    private Integer assigneeId;
-    /**
      * 是否离职，0 在岗
      */
     private String resign;
