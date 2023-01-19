ce85d4fc5740be0c7cc728df0ccfaa39290a9b5b
齐钢 <qigang@mxnavi.com>
2023-01-19 11:18:48 +0800

    update

---
+++ src/main/java/com/q/reminder/reminder/task/redmine/SyncTimeEntryTask.java
@@ -0,0 +1,16 @@
+package com.q.reminder.reminder.task.redmine;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : saiko
+ * @version : v1.0
+ * @ClassName : com.q.reminder.reminder.task.redmine.SyncTimeEntryTask
+ * @Description :
+ * @date :  2023.01.19 11:18
+ */
+@Component
+@Log4j2
+public class SyncTimeEntryTask {
+}
