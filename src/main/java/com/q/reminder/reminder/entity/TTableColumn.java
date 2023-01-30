c3b6f90d3640f40df921f3a0629bfcb481c26cc2
齐钢 <qigang@mxnavi.com>
2023-01-30 09:42:53 +0800

    update

--- src/main/java/com/q/reminder/reminder/entity/TTableColumn.java
+++ src/main/java/com/q/reminder/reminder/entity/TTableColumn.java
@@ -33,7 +33,6 @@ public class TTableColumn extends Model<TTableColumn> implements Serializable {
     @MppMultiId
     private String columnName;
 
-    @TableLogic(value = "0", delval = "1")
     private String enable;
 }
 
