65347b0a4d74bf92a880bb81d5657fb6a06c967c
齐钢 <qigang@mxnavi.com>
2023-02-06 17:02:50 +0800

    update

--- src/main/java/com/q/reminder/reminder/entity/TTableFeatureTmp.java
+++ src/main/java/com/q/reminder/reminder/entity/TTableFeatureTmp.java
@@ -98,6 +98,7 @@ public class TTableFeatureTmp extends Model<TTableFeatureTmp> implements Seriali
     /**
      * 生产发布-时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField(value = "prod_time", fill = FieldFill.INSERT_UPDATE)
     private Date prodTime;
     /**
