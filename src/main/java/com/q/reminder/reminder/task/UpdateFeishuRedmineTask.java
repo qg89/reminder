c1ba7fb87e0e94c0ff00c83666c98d39763e3867
齐钢 <qigang@mxnavi.com>
2023-02-14 09:25:34 +0800

    update123

--- src/main/java/com/q/reminder/reminder/task/UpdateFeishuRedmineTask.java
+++ src/main/java/com/q/reminder/reminder/task/UpdateFeishuRedmineTask.java
@@ -44,7 +44,7 @@ public class UpdateFeishuRedmineTask {
     private Client client;
 
     @XxlJob("everyDaySyncMember")
-    public void everyDaySyncMember() throws Exception {
+    public void everyDaySyncMember123() throws Exception {
         List<GroupInfo> groupToChats = FeishuJavaUtils.getGroupToChats(client);
         List<AdminInfo> adminInfos = adminInfoService.list();
         log.info("获取机器人所在群组信息完成!");
