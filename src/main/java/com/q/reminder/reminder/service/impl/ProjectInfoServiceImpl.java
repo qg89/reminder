29bc722d6fd94f982ca856edfc72dbc2aa830e02
齐钢 <qigang@mxnavi.com>
2023-03-06 17:03:23 +0800

    update ALL

--- src/main/java/com/q/reminder/reminder/service/impl/ProjectInfoServiceImpl.java
+++ src/main/java/com/q/reminder/reminder/service/impl/ProjectInfoServiceImpl.java
@@ -99,7 +99,7 @@ public class ProjectInfoServiceImpl extends ServiceImpl<ProjectInfoMapping, RPro
     }
 
     private Map<String, String> map() {
-        Map<String, String> resMap = new HashMap<>();
+        Map<String, String> resMap = new LinkedHashMap<>();
         resMap.put("pId", "redmine项目ID");
         resMap.put("pKey", "redmine项目key");
         resMap.put("pName", "redmine项目名称");
