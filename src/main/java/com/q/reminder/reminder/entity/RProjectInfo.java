bcd6c796ee2180f1e6fa61a5a08c4ea08cdb2e12
齐钢 <qigang@mxnavi.com>
2023-04-06 17:33:30 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/entity/RProjectInfo.java
+++ src/main/java/com/q/reminder/reminder/entity/RProjectInfo.java
@@ -1,9 +1,6 @@
 package com.q.reminder.reminder.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
 
@@ -114,8 +111,9 @@ public class RProjectInfo extends Model<RProjectInfo> implements Serializable {
      private String redmineType;
     /**
      * 删除标识
-     */     
+     */
      @TableField(value = "is_delete")
+     @TableLogic(value = "0", delval = "1")
      private String isDelete;
          
      @TableField(value = "update_time")
