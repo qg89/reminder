6dd3870679d96583adf8fed33563b6bcebb39899
齐钢 <qigang@mxnavi.com>
2023-04-11 08:43:26 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/ReminderApplication.java
+++ src/main/java/com/q/reminder/reminder/ReminderApplication.java
@@ -1,11 +1,13 @@
 package com.q.reminder.reminder;
 
 import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
+import com.lark.oapi.sdk.servlet.ext.ServletAdapter;
 import com.q.reminder.reminder.config.SpringContextUtils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author saiko
@@ -18,6 +20,11 @@ public class ReminderApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ReminderApplication.class, args);
         SpringContextUtils.setApplicationContext(context);
+    }
+
+    @Bean
+    public ServletAdapter getSelectValidator(){
+        return new ServletAdapter();
     }
 
 }
