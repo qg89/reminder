73a113709aa39fef0624ad2620530332e3a972ab
齐钢 <qigang@mxnavi.com>
2023-07-12 17:44:40 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/controller/FeishuEventController.java
+++ src/main/java/com/q/reminder/reminder/controller/FeishuEventController.java
@@ -15,12 +15,9 @@ import com.lark.oapi.service.drive.v1.model.BitableTableFieldAction;
 import com.lark.oapi.service.drive.v1.model.BitableTableFieldActionValue;
 import com.lark.oapi.service.drive.v1.model.P2FileBitableFieldChangedV1;
 import com.lark.oapi.service.drive.v1.model.P2FileBitableFieldChangedV1Data;
-import com.lark.oapi.service.im.v1.ImService;
-import com.lark.oapi.service.im.v1.model.*;
 import com.q.reminder.reminder.constant.GroupInfoType;
 import com.q.reminder.reminder.entity.FsGroupInfo;
 import com.q.reminder.reminder.entity.TableFieldsFeature;
-import com.q.reminder.reminder.entity.UserMemgerInfo;
 import com.q.reminder.reminder.service.GroupInfoService;
 import com.q.reminder.reminder.service.TableFieldsFeatureService;
 import com.q.reminder.reminder.service.UserMemberService;
@@ -134,7 +131,7 @@ public class FeishuEventController {
                         String afterValue = j.getString("after_value");
                         String beforeValue = j.getString("before_value");
                         String recordId = j.getString("record_id");
-//                        System.out.println(j);
+                        System.out.println(j);
                     }
                 }
             })
@@ -157,35 +154,35 @@ public class FeishuEventController {
                 }
             })
             // 会话成员变更事件
-            .onP2ChatMemberUserAddedV1(new ImService.P2ChatMemberUserAddedV1Handler() {
-                @Override
-                public void handle(P2ChatMemberUserAddedV1 event) throws Exception {
-                    P2ChatMemberUserAddedV1Data eventData = event.getEvent();
-                    String chatId = eventData.getChatId();
-                    if (Objects.equals(CHAT_ID, chatId)) {
-                        ChatMemberUser[] users = eventData.getUsers();
-                        for (ChatMemberUser user : users) {
-                            UserMemgerInfo info = new UserMemgerInfo();
-                            info.setName(user.getName());
-                            info.setMemberId(user.getUserId().getOpenId());
-                            info.setTenantKey(user.getTenantKey());
-                            userMemberService.saveOrUpdate(info);
-                        }
-                    }
-                }
-            })
-            // 会话成员删除事件
-            .onP2ChatMemberUserDeletedV1(new ImService.P2ChatMemberUserDeletedV1Handler() {
-                @Override
-                public void handle(P2ChatMemberUserDeletedV1 event) throws Exception {
-                    P2ChatMemberUserDeletedV1Data eventData = event.getEvent();
-                    String chatId = eventData.getChatId();
-                    if (Objects.equals(CHAT_ID, chatId)) {
-                        for (ChatMemberUser user : eventData.getUsers()) {
-                            userMemberService.remove(Wrappers.<UserMemgerInfo>lambdaUpdate().eq(UserMemgerInfo::getMemberId, user.getUserId().getOpenId()));
-                        }
-                    }
-                }
-            })
+//            .onP2ChatMemberUserAddedV1(new ImService.P2ChatMemberUserAddedV1Handler() {
+//                @Override
+//                public void handle(P2ChatMemberUserAddedV1 event) throws Exception {
+//                    P2ChatMemberUserAddedV1Data eventData = event.getEvent();
+//                    String chatId = eventData.getChatId();
+//                    if (Objects.equals(CHAT_ID, chatId)) {
+//                        ChatMemberUser[] users = eventData.getUsers();
+//                        for (ChatMemberUser user : users) {
+//                            UserMemgerInfo info = new UserMemgerInfo();
+//                            info.setName(user.getName());
+//                            info.setMemberId(user.getUserId().getOpenId());
+//                            info.setTenantKey(user.getTenantKey());
+//                            userMemberService.saveOrUpdate(info);
+//                        }
+//                    }
+//                }
+//            })
+//            // 会话成员删除事件
+//            .onP2ChatMemberUserDeletedV1(new ImService.P2ChatMemberUserDeletedV1Handler() {
+//                @Override
+//                public void handle(P2ChatMemberUserDeletedV1 event) throws Exception {
+//                    P2ChatMemberUserDeletedV1Data eventData = event.getEvent();
+//                    String chatId = eventData.getChatId();
+//                    if (Objects.equals(CHAT_ID, chatId)) {
+//                        for (ChatMemberUser user : eventData.getUsers()) {
+//                            userMemberService.remove(Wrappers.<UserMemgerInfo>lambdaUpdate().eq(UserMemgerInfo::getMemberId, user.getUserId().getOpenId()));
+//                        }
+//                    }
+//                }
+//            })
             .build();
 }
