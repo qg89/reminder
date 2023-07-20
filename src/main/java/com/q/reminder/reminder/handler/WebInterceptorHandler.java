da8179e3218094ab9a0f5cc5e9f8c24c493f0875
齐钢 <qigang@mxnavi.com>
2023-07-20 18:20:39 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/handler/WebInterceptorHandler.java
+++ src/main/java/com/q/reminder/reminder/handler/WebInterceptorHandler.java
@@ -1,16 +1,20 @@
 package com.q.reminder.reminder.handler;
 
-import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson2.JSONObject;
 import com.q.reminder.reminder.mapper.UserInfoMapping;
 import com.q.reminder.reminder.service.LoginService;
 import com.q.reminder.reminder.util.IpUtils;
+import com.q.reminder.reminder.util.JWTUtil;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -32,27 +36,57 @@ public class WebInterceptorHandler implements HandlerInterceptor {
     private UserInfoMapping userInfoMapping;
     @Autowired
     private LoginService loginService;
+    @Autowired
+    private JWTUtil jjwtUtil;
 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String strToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.isNotBlank(strToken)) {
+            Claims claims = jjwtUtil.getTokenPayLoad(strToken);
+            String userToken = claims.toString();
+            Integer userID = Integer.getInteger(claims.getId());
+            request.setAttribute("LOGIN_USER_ID", userID);
+            request.setAttribute("LOGIN_USER_TOKEN", userToken);
+        }
+
+
         String verifySignParam = getVerifySignParam(request);
         MethodParameter[] methodParameters = ((HandlerMethod) handler).getMethodParameters();
-        if (!JSONUtil.isTypeJSON(verifySignParam) || StringUtils.isBlank(verifySignParam)) {
-            return false;
+        String username;
+        if (StringUtils.isNotBlank(verifySignParam)) {
+            username = JSONObject.parseObject(verifySignParam).getString("username");
+        } else {
+            username = getCurrentLoginUser();
         }
-        String username = JSONObject.parse(verifySignParam).getString("username");
-        String remoteAddr = IpUtils.getIp(request);
-        log.info("remoteAddr:{}", remoteAddr);
+        boolean flag = true;
+        if (StringUtils.isBlank(username)) {
+           return false;
+        }
         String ip = loginService.getByUserNameToIp(username);
+        String remoteAddr = IpUtils.getIp(request);
         if (!remoteAddr.contains(ip)) {
-            return false;
+            flag = false;
         }
-        return true;
+        return flag;
     }
 
     public String getVerifySignParam(HttpServletRequest request) throws IOException {
         BodyHttpServletRequestWrapper bodyHttpServletRequestWrapper = (BodyHttpServletRequestWrapper) request;
         return bodyHttpServletRequestWrapper.getBody();
+    }
+
+    /**
+     * 获取当前登录用户
+     * @return
+     */
+    private String getCurrentLoginUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new RuntimeException("当前无用户登录");
+        } else {
+            return authentication.getName();
+        }
     }
 }
