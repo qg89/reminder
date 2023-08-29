1cb12eb276f7c615abe4a8a5efaef91678513cf4
齐钢 <qigang@mxnavi.com>
2023-08-29 11:22:13 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/task/me/ReminderTask.java
+++ src/main/java/com/q/reminder/reminder/task/me/ReminderTask.java
@@ -1,14 +1,14 @@
 package com.q.reminder.reminder.task.me;
 
 import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.NumberUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.nlf.calendar.Holiday;
+import com.nlf.calendar.util.HolidayUtil;
 import com.q.reminder.reminder.constant.FeiShuContents;
 import com.q.reminder.reminder.entity.RdTimeEntry;
-import com.q.reminder.reminder.entity.SHolidayConfig;
 import com.q.reminder.reminder.service.RdTimeEntryService;
-import com.q.reminder.reminder.service.SHolidayConfigService;
 import com.q.reminder.reminder.util.feishu.BaseFeishu;
 import com.q.reminder.reminder.vo.MessageVo;
 import lombok.AllArgsConstructor;
@@ -18,6 +18,7 @@ import tech.powerjob.worker.core.processor.TaskContext;
 import tech.powerjob.worker.core.processor.sdk.BasicProcessor;
 import tech.powerjob.worker.log.OmsLogger;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,28 +33,29 @@ import java.util.List;
 public class ReminderTask implements BasicProcessor {
 
     private final RdTimeEntryService rdTimeEntryService;
-    private final SHolidayConfigService sHolidayConfigService;
 
     @Override
     public ProcessResult process(TaskContext context) throws Exception {
         OmsLogger omsLogger = context.getOmsLogger();
         ProcessResult result = new ProcessResult(true);
-
-        String date = DateUtil.date().offsetNew(DateField.DAY_OF_MONTH, -2).toString("yyyy-MM-dd");
-        SHolidayConfig holidayConfig = sHolidayConfigService.getOne(Wrappers.<SHolidayConfig>lambdaQuery().eq(SHolidayConfig::getDate, date));
-        if (holidayConfig == null || holidayConfig.getWork() != 1) {
-            omsLogger.info("没有找到对应的日报配置,{}", holidayConfig);
-            return result;
+        DateTime dateTime = DateUtil.offsetDay(new Date(), -1);
+        String date = dateTime.toString("yyyy-MM-dd");
+        // 判断周末
+        while (DateUtil.isWeekend(dateTime)) {
+            dateTime.offset(DateField.DAY_OF_MONTH, -1);
         }
-        List<RdTimeEntry> list = rdTimeEntryService.list(Wrappers.<RdTimeEntry>lambdaQuery().eq(RdTimeEntry::getSpentOn, date).eq(RdTimeEntry::getUserid, 1215));
-        double value = NumberUtil.round(list.stream().mapToDouble(RdTimeEntry::getHours).sum(), 2).doubleValue();
-        if (value < 8) {
-            MessageVo sendVo = new MessageVo();
-            sendVo.setReceiveId(FeiShuContents.ADMIN_MEMBERS);
-            sendVo.setContent("Hi 同学，该写日报了，" + date + "！已填日报：" + value + " 小时");
-            BaseFeishu.message().sendText(sendVo, omsLogger);
-        } else {
-            omsLogger.info("日报已填写~！ date：{}, value：{}, {}", date, value, holidayConfig);
+        Holiday holiday = HolidayUtil.getHoliday(date);
+        if (holiday != null && holiday.isWork()) {
+            List<RdTimeEntry> list = rdTimeEntryService.list(Wrappers.<RdTimeEntry>lambdaQuery().eq(RdTimeEntry::getSpentOn, date).eq(RdTimeEntry::getUserid, 1215));
+            double value = list.stream().mapToDouble(RdTimeEntry::getHours).sum();
+            if (value < 8) {
+                MessageVo sendVo = new MessageVo();
+                sendVo.setReceiveId(FeiShuContents.ADMIN_MEMBERS);
+                sendVo.setContent("Hi 同学，该写日报了，" + date + "！已填日报：" + value + " 小时");
+                BaseFeishu.message().sendText(sendVo, omsLogger);
+            } else {
+                omsLogger.info("日报已填写~！ date：{}, value：{}", date, value);
+            }
         }
         return result;
     }
