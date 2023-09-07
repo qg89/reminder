f4845e1fe867932d172647bc31e84b9d437973d4
qig <qig@meixing.com>
2023-09-07 10:14:16 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/task/me/ReminderTask.java
+++ src/main/java/com/q/reminder/reminder/task/me/ReminderTask.java
@@ -1,8 +1,12 @@
 package com.q.reminder.reminder.task.me;
 
+import com.q.reminder.reminder.exception.FeishuException;
 import com.q.reminder.reminder.service.ReminderUserConfService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import tech.powerjob.client.PowerJobClient;
+import tech.powerjob.common.response.JobInfoDTO;
+import tech.powerjob.common.response.ResultDTO;
 import tech.powerjob.worker.core.processor.ProcessResult;
 import tech.powerjob.worker.core.processor.TaskContext;
 import tech.powerjob.worker.core.processor.sdk.BasicProcessor;
@@ -19,14 +23,21 @@ import tech.powerjob.worker.log.OmsLogger;
 @AllArgsConstructor
 public class ReminderTask implements BasicProcessor {
 
-
+    private final PowerJobClient client;
     private final ReminderUserConfService reminderUserConfService;
 
     @Override
-    public ProcessResult process(TaskContext context) throws Exception {
+    public ProcessResult process(TaskContext context) {
         OmsLogger omsLogger = context.getOmsLogger();
         ProcessResult result = new ProcessResult(true);
-        reminderUserConfService.reminder(omsLogger);
+        try {
+            reminderUserConfService.reminder(omsLogger);
+        } catch (Exception e) {
+            result.setSuccess(false);
+            ResultDTO<JobInfoDTO> resultDTO = client.fetchJob(context.getJobId());
+            String taskName = resultDTO.getData().getJobName();
+            throw new FeishuException(e, taskName + "- 异常");
+        }
         return result;
     }
 }
