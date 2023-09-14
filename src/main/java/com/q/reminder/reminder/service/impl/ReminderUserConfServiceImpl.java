736a848c6e4b81dba4d0e05de9e06bf9e1b9f5f3
qig <qig@meixing.com>
2023-09-14 13:38:03 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/service/impl/ReminderUserConfServiceImpl.java
+++ src/main/java/com/q/reminder/reminder/service/impl/ReminderUserConfServiceImpl.java
@@ -34,25 +34,25 @@ public class ReminderUserConfServiceImpl extends ServiceImpl<ReminderUserConfMap
     public void reminder(OmsLogger omsLogger) {
         final int normal = 8;
         DateTime now = DateTime.now();
-        // 当前周一日期
+        // 开始日期
         DateTime dateTime = now.offsetNew(DateField.DAY_OF_MONTH, -7);
         List<UserReminderVo> userReminderList = baseMapper.listByUser(dateTime.toString("yyyy-MM-dd"));
-        userReminderList.removeIf(e -> {
-            String memberId = null;
-            boolean flag = Objects.equals("1", e.getEnable());
-            if (flag) {
-                return true;
-            }
-            Date toDay = new DateTime(now.toString("yyyy-MM-dd"));
-            Date startDate = e.getStartDate();
-            Date endDate = e.getEndDate();
-            // 当天在开始结束范围内，过滤掉
-            if (startDate != null && endDate != null && DateUtil.isIn(toDay, startDate, endDate)) {
-                omsLogger.info("不提醒，[{}]当前为休假期，startDate：{} ~ endDate：{}", e.getUserName(), startDate, endDate);
-                return true;
-            }
-            return false;
-        });
+//        userReminderList.removeIf(e -> {
+//            String memberId = null;
+//            boolean flag = Objects.equals("1", e.getEnable());
+//            if (flag) {
+//                return true;
+//            }
+//            Date toDay = new DateTime(now.toString("yyyy-MM-dd"));
+//            Date startDate = e.getStartDate();
+//            Date endDate = e.getEndDate();
+//            // 当天在开始结束范围内，过滤掉
+//            if (startDate != null && endDate != null && DateUtil.isIn(toDay, startDate, endDate)) {
+//                omsLogger.info("不提醒，[{}]当前为休假期，startDate：{} ~ endDate：{}", e.getUserName(), startDate, endDate);
+//                return true;
+//            }
+//            return false;
+//        });
         if (CollectionUtils.isEmpty(userReminderList)) {
             omsLogger.info("当前日期全部填写日报！");
             return;
@@ -66,7 +66,8 @@ public class ReminderUserConfServiceImpl extends ServiceImpl<ReminderUserConfMap
                 omsLogger.info("获取用户信息为空！");
                 return;
             }
-
+            Date startDate = userReminderVo.getStartDate();
+            Date endDate = userReminderVo.getEndDate();
             StringBuffer content = new StringBuffer("Hi 同学，该写日报了！").append("\r\t\n");
             // 按日期
             Map<String, List<UserReminderVo>> spentMap = list.stream().collect(Collectors.groupingBy(UserReminderVo::getSpentOn));
@@ -75,6 +76,11 @@ public class ReminderUserConfServiceImpl extends ServiceImpl<ReminderUserConfMap
             spentMap.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(e -> resultMap.put(e.getKey(), e.getValue()));
             AtomicInteger i = new AtomicInteger();
             resultMap.forEach((date, userList) -> {
+                // 休假不提醒
+                if (startDate != null && endDate != null && DateUtil.isIn(new DateTime(date).toJdkDate(), startDate, endDate)) {
+                    omsLogger.info("不提醒，[{}]当前为休假期，startDate：{} ~ endDate：{}", userReminderVo.getUserName(), startDate, endDate);
+                    return;
+                }
                 // 当日日报合计
                 double sumDay = userList.stream().filter(e -> e.getHours() != null).mapToDouble(UserReminderVo::getHours).sum();
                 // 判断是否 < 8
