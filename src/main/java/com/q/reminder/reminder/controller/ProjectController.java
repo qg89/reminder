a2c0d501d52c85bd73127776f2ee82822b627bf3
qig <qig@meixing.com>
2023-10-20 18:23:00 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/controller/ProjectController.java
+++ src/main/java/com/q/reminder/reminder/controller/ProjectController.java
@@ -128,7 +128,8 @@ public class ProjectController {
     @GetMapping("/so")
     public ReturnT<String> so() {
         log.info("init - system loadLibary");
-        System.loadLibrary("libcurl_feish");
+//        System.loadLibrary("libcurl_feish");
+        System.load("/usr/java/openjdk-17/include/linux/libcurl_feish.so");
         log.info("init - so success");
         Demo123 demo123 = new Demo123();
         String i = demo123.sayHello();
