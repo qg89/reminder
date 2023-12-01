69d2024ae9fbf8b3c2a4f9c0138d75e261bb0388
qig <qigang@meixing.com>
2023-12-01 13:32:12 +0800

    revert:
    [skip ci]

---
+++ src/main/java/com/q/reminder/reminder/task/me/ReminderMeDeleteMessageTask.java
@@ -0,0 +1,35 @@
+package com.q.reminder.reminder.task.me;
+
+import com.q.reminder.reminder.constant.RedisKeyContents;
+import com.q.reminder.reminder.util.feishu.BaseFeishu;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import tech.powerjob.worker.core.processor.ProcessResult;
+import tech.powerjob.worker.core.processor.TaskContext;
+import tech.powerjob.worker.core.processor.sdk.BasicProcessor;
+import tech.powerjob.worker.log.OmsLogger;
+
+/**
+ * @author : Administrator
+ * @version : v1.0
+ * @ClassName : com.q.reminder.reminder.task.me.ReminderMeTask
+ * @Description :
+ * @date :  2023.11.29 12:53
+ */
+@Component
+@AllArgsConstructor
+public class ReminderMeDeleteMessageTask implements BasicProcessor {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public ProcessResult process(TaskContext context) throws Exception {
+        OmsLogger omsLogger = context.getOmsLogger();
+        Object messageId = redisTemplate.opsForValue().get(RedisKeyContents.FEISHU_MESSAGE_FEISHUMESSAGE);
+        if (messageId != null) {
+            BaseFeishu.groupMessage().deleteMessage((String) messageId);
+        }
+        omsLogger.info("消息已撤回：{}", messageId);
+        return new ProcessResult(true);
+    }
+}
