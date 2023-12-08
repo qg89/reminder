e2520114f93d1400a2dbd4b2b2557ccbe252b94f
qig <qigang@meixing.com>
2023-12-08 16:42:35 +0800

    revert:
    [skip ci]

--- src/main/java/com/q/reminder/reminder/ReminderApplication.java
+++ src/main/java/com/q/reminder/reminder/ReminderApplication.java
@@ -2,6 +2,7 @@ package com.q.reminder.reminder;
 
 import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import com.q.reminder.reminder.util.SpringContextUtils;
+import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 /**
  * @author saiko
  */
+@Log4j2
 @SpringBootApplication
 @MapperScan("com.q.reminder.reminder.mapper")
 @EnableMPP
@@ -18,5 +20,12 @@ public class ReminderApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ReminderApplication.class, args);
         SpringContextUtils.setApplicationContext(context);
+//        if (SystemUtils.isLinux()) {
+//            try {
+//                System.load("/usr/java/openjdk-17/include/linux/ibfeishutoken.so");
+//            } catch (Exception e) {
+//                log.error("init lib failed", e);
+//            }
+//        }
     }
 }
